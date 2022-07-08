7b6069abc8dbf6e0f4964f0b03fe74e9612031ec
Septian Adhitya <dsrisk911@gmail.com>
2022-07-08 15:05:29 +0700

    add route get media

--- routes/media.js
+++ routes/media.js
@@ -5,6 +5,20 @@ const base64Img = require("base64-img");
 
 const { Media } = require("../models");
 
+router.get("/", async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ["id", "image"],
+  });
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get("host")}/${m.image}`;
+    return m;
+  });
+  return res.json({
+    status: "success",
+    data: mappedMedia,
+  });
+});
+
 router.post("/", (req, res) => {
   const image = req.body.image;
 
@@ -25,7 +39,7 @@ router.post("/", (req, res) => {
 
     const filename = filepath.split("/").pop();
 
-    const media = await Media.create({ image: `image/${filename}` });
+    const media = await Media.create({ image: `images/${filename}` });
     return res.json({
       status: "success",
       data: {
